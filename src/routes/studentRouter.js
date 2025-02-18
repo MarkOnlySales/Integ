2990e2e7d09e106401cb1dd3797248ff3fec98d7
MarkOnlySales <mark@onlysales.io>
2025-02-18 16:04:10 +0800

    add route validation

--- src/routes/studentRouter.js
+++ src/routes/studentRouter.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const studentController = require("../controllers/studentController");
+const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
 
 router.get("/", studentController.getData)
 
-router.post("/", studentController.postData)
+router.post("/", 
+    body("firstName").isString().isLength({ min: 3 }),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error: "Invalid firstName"})
+        }
+    },
+    body("lastName").isString().isLength({ min: 3 }),
+    studentController.postData)
 
 router.patch("/:id", studentController.updateData)
 
